b14393b3cfa55674f4e4b7832647ef31f776bd00
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2021-04-14 23:53:53 -0300

    remocao dos arquivos inuteis e adicao do meu teste

--- Solução base/Program.cs
+++ Solução base/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using System.Numerics;
+using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace tp_torneio
 {
@@ -7,8 +9,65 @@ namespace tp_torneio
     {
         static void Main(string[] args)
         {
+            //TESTE LUCAS
+            var n = 5;
+            var k = 2;
+            var srcOriginal = new List<int[]> { new int[] { 2, 10, 0 },
+                                        new int[] { 2, 10, 0 },
+                                        new int[] { 1, 1, 0 },
+                                        new int[] { 3, 1, 0 },
+                                        new int[] { 3, 1, 0 }};
+            srcOriginal = srcOriginal.OrderByDescending(x => x[0]).ThenBy(x => x[1]).ToList();
+            int index = 0;
+            int edInicial = 0;
+            var ed = new int[] { 0, edInicial, -1 };
+            var src = new List<int[]>();
+            do
+            {
+                src.Remove(ed);
+                if (index == 0)
+                {
+                    ed = new int[] { 0, edInicial, -1 };
+                    src = new List<int[]> { new int[] { 2, 10, 0 },
+                                        new int[] { 2, 10, 0 },
+                                        new int[] { 1, 1, 0 },
+                                        new int[] { 3, 1, 0 },
+                                        new int[] { 3, 1, 0 }};
+                    src = src.OrderByDescending(x => x[0]).ThenBy(x => x[1]).ToList();
+                }
+                if (ed[1] == 0 || index == n)
+                {
+                    AddPontos(src, index);
+                    src.Add(ed);
+                    if (!ObjetivoCompleto(src, k))
+                    {
+                        src.Remove(ed);
+                        edInicial += src[index][1];
+                        index = 0;
+                    }
+                    else break;
+                }
+                else
+                {
+                    if (ed[1] >= src[index][1])
+                    {
+                        ++ed[0];
+                        ed[1] -= src[index][1];
+                    }
+                    else
+                    {
+                        ++src[index][0];
+                        ++src[index][2];
+                    }
+                    ++index;
+                }
+            } while (index < n);
 
+            Console.WriteLine($"Objetivo completo com {edInicial} de mínimo de esforço!");
+            Console.WriteLine("Placar: ");
+            //fim TESTE LUCAS
 
+            /*
             int N = int.Parse(Console.ReadLine());
             int K = int.Parse(Console.ReadLine());
             int[] pi = new int[N];
@@ -39,8 +98,26 @@ namespace tp_torneio
             if (result < 0) {
                 result = -1;
             }
-            Console.WriteLine(result + "resultado");
+            Console.WriteLine(result + "resultado");*/
             Console.ReadKey();
+        }
+
+        private static bool ObjetivoCompleto(List<int[]> src, int k)
+        {
+            var src2 = src.OrderByDescending(x => x[0]).ThenBy(x => x[2]).ToList();
+            var sad = src2.IndexOf(src2.First(x => x[2] == -1));
+            if (sad <= k-1)
+                return true;
+            else return false;
+        }
+
+        private static void AddPontos(List<int[]> src, int index)
+        {
+            for (int i = index; i < src.Count; ++i)
+            {
+                ++src[i][0];
+                ++src[i][2];
+            }
         }
     }
 }
