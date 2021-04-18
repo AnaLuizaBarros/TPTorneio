88b8de9a0142bc4d07afacd53539c33b36af479c
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2021-04-17 21:08:11 -0300

    versao final Kappa

--- Solução base/Program.cs
+++ Solução base/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace tp_torneio
 {
@@ -9,50 +10,64 @@ namespace tp_torneio
     {
         static void Main(string[] args)
         {
-            //TESTE LUCAS
-            var n = 5;
-            var k = 2;
-            var srcOriginal = new List<int[]> { new int[] { 2, 10, 0 },
-                                        new int[] { 2, 10, 0 },
-                                        new int[] { 1, 1, 0 },
-                                        new int[] { 3, 1, 0 },
-                                        new int[] { 3, 1, 0 }};
-            srcOriginal = srcOriginal.OrderByDescending(x => x[0]).ThenBy(x => x[1]).ToList();
+            foreach (int n in new int[] { 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1000, 10000, 100000 })
+            {
+                Console.WriteLine($"Programa iniciado com {n} participantes!");
+                Iterativa(n);
+                Console.WriteLine("Pressione qualquer tecla para executar o próximo N!\n");
+                Console.ReadKey();
+            }
+        }
+
+        private static void Iterativa(int n)
+        {
+            Random rnd = new Random();
+            Stopwatch stopWatch = new Stopwatch();
+            
+            int k = rnd.Next(1, n+1);
+            var srcOriginal = GerarPlacar(n);
             int index = 0;
-            int edInicial = 0;
-            var ed = new int[] { 0, edInicial, -1 };
-            var src = new List<int[]>();
+            long edInicial = 0;
+            var ducan = new long[] { 0, edInicial, 1, -1 };
+            var src = new List<long[]>();
+            bool completed = false;
+
+            ImprimirInicio(srcOriginal, k);
+
+            stopWatch.Start();
+
             do
             {
-                src.Remove(ed);
+                src.Remove(ducan);
                 if (index == 0)
                 {
-                    ed = new int[] { 0, edInicial, -1 };
-                    src = new List<int[]> { new int[] { 2, 10, 0 },
-                                        new int[] { 2, 10, 0 },
-                                        new int[] { 1, 1, 0 },
-                                        new int[] { 3, 1, 0 },
-                                        new int[] { 3, 1, 0 }};
-                    src = src.OrderByDescending(x => x[0]).ThenBy(x => x[1]).ToList();
+                    ducan = new long[] { 0, edInicial, 1, -1 };
+                    src = ClonarLista(srcOriginal);
                 }
-                if (ed[1] == 0 || index == n)
+                if (ducan[1] == 0 || index == n)
                 {
                     AddPontos(src, index);
-                    src.Add(ed);
-                    if (!ObjetivoCompleto(src, k))
+                    src.Add(ducan);
+                    var final = new List<long[]>();
+                    if (!ObjetivoCompleto(src, k, out final))
                     {
-                        src.Remove(ed);
+                        src.Remove(ducan);
                         edInicial += src[index][1];
                         index = 0;
                     }
-                    else break;
+                    else
+                    {
+                        completed = true;
+                        ImprimirObjetivoCompleto(edInicial, final);
+                        break;
+                    }
                 }
                 else
                 {
-                    if (ed[1] >= src[index][1])
+                    if (ducan[1] >= src[index][1])
                     {
-                        ++ed[0];
-                        ed[1] -= src[index][1];
+                        ++ducan[0];
+                        ducan[1] -= src[index][1];
                     }
                     else
                     {
@@ -63,61 +78,88 @@ namespace tp_torneio
                 }
             } while (index < n);
 
-            Console.WriteLine($"Objetivo completo com {edInicial} de mínimo de esforço!");
-            Console.WriteLine("Placar: ");
-            //fim TESTE LUCAS
+            stopWatch.Stop();
 
-            /*
-            int N = int.Parse(Console.ReadLine());
-            int K = int.Parse(Console.ReadLine());
-            int[] pi = new int[N];
-            int[] ei = new int[N];
-            int p = 0;
-            int e = 0;
-            for (int i = 0; i < (N*2); i++)
-            {
-                if (i % 2 == 0)
-                {
-                    ei[e] = int.Parse(Console.ReadLine());
-                    e++;
-                }
-                else {
-                    pi[p] = int.Parse(Console.ReadLine());
-                    p++;
-                }
-            }
-            ei = ei.OrderByDescending(i => i).ToArray();
-            pi = pi.OrderByDescending(i => i).ToArray();
-            int result = 0;
-            for (int i = 0; i < N; i++)
-            {
-                Console.WriteLine(pi[i] + " pi");
-                Console.WriteLine(ei[i] + " ei");
-                result += pi[i] - ei[i]; 
-            }
-            if (result < 0) {
-                result = -1;
-            }
-            Console.WriteLine(result + "resultado");*/
-            Console.ReadKey();
+            ImprimirFinal(n, k, stopWatch, completed);
         }
 
-        private static bool ObjetivoCompleto(List<int[]> src, int k)
+        private static bool ObjetivoCompleto(List<long[]> src, long k, out List<long[]> ord)
         {
-            var src2 = src.OrderByDescending(x => x[0]).ThenBy(x => x[2]).ToList();
-            var sad = src2.IndexOf(src2.First(x => x[2] == -1));
-            if (sad <= k-1)
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            var ordenado = src.OrderByDescending(x => x[0]).ThenByDescending(x => x[2]).ToList();
+            var index = ordenado.IndexOf(ordenado.First(x => x[3] == -1));
+            //sw.Stop();
+            //Console.WriteLine($"ObjetivoCompleto Benchmark: {sw.Elapsed}");
+            if (index <= k - 1)
+            {
+                ord = ordenado;
                 return true;
-            else return false;
+            }
+            else
+            {
+                ord = src;
+                return false;
+            }
         }
 
-        private static void AddPontos(List<int[]> src, int index)
+        private static void AddPontos(List<long[]> src, int index)
         {
             for (int i = index; i < src.Count; ++i)
             {
                 ++src[i][0];
                 ++src[i][2];
             }
+        }
+
+        private static List<long[]> ClonarLista(List<long[]> src)
+        {
+            return src.Select(item => (long[])item.Clone()).ToList();
+        }
+
+        private static void ImprimirMatriz(List<long[]> src)
+        {
+            foreach (long[] array in src)
+            {
+                for(long i = 0; i < 2; i++)
+                    Console.Write(array[i] + "\t\t" + (array[i+2] == -1 ? "<- Ducan\t" : ""));
+                Console.WriteLine();
+            }
+        }
+
+        private static void ImprimirObjetivoCompleto(long edInicial, List<long[]> final)
+        {
+            Console.WriteLine($"Objetivo completo com {edInicial} de mínimo de esforço!");
+            Console.WriteLine("Placar: ");
+            Console.WriteLine("Pontuação\tEsforço");
+            final = final.Select(x => {
+                if (x[3] == -1) x[1] = edInicial; return x;
+            }).ToList();
+            ImprimirMatriz(final);
+        }
+
+        private static void ImprimirInicio(List<long[]> srcOriginal, long k)
+        {
+            Console.WriteLine("Placar inicial");
+            Console.WriteLine("Pontuação\tEsforço");
+            ImprimirMatriz(srcOriginal);
+            Console.WriteLine($"Objetivo de Ducan: {k}");
+        }
+
+        private static List<long[]> GerarPlacar(int n)
+        {
+            Random rnd = new Random();
+            var src = new List<long[]>();
+            for(int i = 0; i < n; ++i)
+                src.Add(new long[] { rnd.Next(0, n), rnd.Next(0, 100000), 0, -2 });
+            return src.OrderByDescending(x => x[0]).ThenBy(x => x[1]).ToList(); ;
+        }
+
+        private static void ImprimirFinal(int n, int k, Stopwatch stopWatch, bool completed)
+        {
+            Console.WriteLine($"Tempo de Execução com {n} participantes com o objetivo de estar em {k}º lugar: " + stopWatch.Elapsed);
+            if (!completed)
+                Console.WriteLine("Esforço: -1");
         }
     }
 }
